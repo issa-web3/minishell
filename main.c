46ae34f062ff2454aa531849cc496d9cf02d7668
Khalid  Oukoujane <khoukouj@e1r12p3.1337.ma>
2025-02-03 14:11:31 +0100

    first commit

---
+++ main.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: khoukouj <khoukouj@student.42.fr>          #+#  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025-02-03 12:59:19 by khoukouj          #+#    #+#             */
+/*   Updated: 2025-02-03 12:59:19 by khoukouj         ###   ########.1337.ma  */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+
+
+int main(int ac, char **av, char **env)
+{
+	char	*line;
+
+	while (1)
+	{
+		line = readline("RICH > ");
+		// data = parser(ac, av, env, line);
+		// execute(data);
+	}
+}
---
+++ minishell.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: khoukouj <khoukouj@student.42.fr>          #+#  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025-02-03 12:59:53 by khoukouj          #+#    #+#             */
+/*   Updated: 2025-02-03 12:59:53 by khoukouj         ###   ########.1337.ma  */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+
+#endif
