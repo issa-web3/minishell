84c95d9980b4dd4e6d0f04c3dc38608bf30a5697
Khalid  Oukoujane <khoukouj@e1r10p1.1337.ma>
2025-02-05 15:05:29 +0100

    added structs.h

--- structs.h
+++ structs.h
@@ -13,10 +13,23 @@
 #ifndef STRUCTS_H
 # define STRUCTS_H
 
+typedef struct s_file
+{
+	char	*name;
+	int		type;
+}			t_file;
+
+typedef struct s_token
+{
+	char	*token;
+	int		type;
+}			t_token;
+
+// ls -l -a < in1 > out1 << here1 | grep "a" > out | end
 typedef struct s_2_exec
 {
 	char			**cmd;
-	char			*file;
+	t_file			*files;
 	struct s_2_exec	*next;
 }				t_2_exec;
 
