74eb233e48fbdffd6d5b884ffc45529dad6cc950
Issa Oulkhir <aissaoulkhir22@gmail.com>
2025-02-08 00:07:05 +0100

    echo, export, env, unset ---> need one thing

--- executer/execute.c
+++ executer/execute.c
@@ -6,7 +6,7 @@
 /*   By: ioulkhir <ioulkhir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/04 09:01:06 by ioulkhir          #+#    #+#             */
-/*   Updated: 2025/02/07 14:10:06 by ioulkhir         ###   ########.fr       */
+/*   Updated: 2025/02/08 00:06:18 by ioulkhir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,7 @@ void	exec_by_idx(t_2_exec *data, char **env, int i)
 	cmd = data->cmd[0];
 	data->cmd[0] = get_path(data->cmd[0]);
 	execve(data->cmd[0], data->cmd, NULL);
-	if (dup2(STDERR_FILENO, STDOUT_FILENO) == -1)
-		perror("dup2");
+	// stderror please
 	printf("%s: command not found\n", cmd);
 }
 
