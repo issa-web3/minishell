a72bb8d41fd4b3d1484d284add05985d8c17c80c
Issa Oulkhir <aissaoulkhir22@gmail.com>
2025-02-07 13:58:50 +0100

    built-ins ...

--- executer/built-ins/cd.c
+++ executer/built-ins/cd.c
@@ -6,7 +6,7 @@
 /*   By: ioulkhir <ioulkhir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/06 13:50:34 by ioulkhir          #+#    #+#             */
-/*   Updated: 2025/02/07 13:39:59 by ioulkhir         ###   ########.fr       */
+/*   Updated: 2025/02/07 13:57:29 by ioulkhir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,14 @@ void	ft_cd(char **cmd, char **env)
 	char	buffer[1024];
 
 	if (cmd[1] == NULL)
-		chdir("/");
-	getcwd(buffer, 1024);
-	if (chdir(ft_strjoin(ft_strjoin(ft_strdup(buffer), "/"), cmd[1])) == -1)
-		perror("cd");
+	{
+		if (chdir("/") == -1)
+			perror("cd");
+	}
+	else
+	{
+		getcwd(buffer, 1024);
+		if (chdir(ft_strjoin(ft_strjoin(ft_strdup(buffer), "/"), cmd[1])) == -1)
+			perror("cd");
+	}
 }
