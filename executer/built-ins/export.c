74f55beadfa208d4dece99344a293d882d6c8e7a
Issa Oulkhir <ioulkhir@e1r10p12.1337.ma>
2025-03-12 08:43:30 +0000

    export exit cd

--- executer/built-ins/export.c
+++ executer/built-ins/export.c
@@ -6,7 +6,7 @@
 /*   By: ioulkhir <ioulkhir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/06 13:50:34 by ioulkhir          #+#    #+#             */
-/*   Updated: 2025/02/13 14:25:42 by ioulkhir         ###   ########.fr       */
+/*   Updated: 2025/03/12 08:41:58 by ioulkhir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,17 +21,15 @@ void	ft_export(char **cmd, t_env **my_env, t_garbage **my_garbage)
 	if (cmd[1] == NULL)
 		return ; // segma
 	parsed = ft_split(cmd[1], '=', my_garbage);
-	if (parsed == NULL)
+	if (parsed == NULL || parsed[0] == NULL || parsed[1] == NULL)
+	{
+		write(2, "syntax issue\n", 13);
 		return ;
-	if (parsed[0] == NULL)
-		return ;
-	if (parsed[1] == NULL)
-		return ;
+	}
 	append = 0;
 	if (parsed[0][ft_strlen(parsed[0]) - 1] == '+')
 		(parsed[0][ft_strlen(parsed[0]) - 1] = 0, append = 1);
 	to_export = ft_getenv(parsed[0], my_env);
-	// append or overwrite
 	if (to_export == NULL)
 		append_env(my_env, get_last_env(my_env), new_env_var(parsed, my_garbage));
 	else if (append)
