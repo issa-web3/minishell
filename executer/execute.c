e5ac2a52e02be57c4814d2fef3d38bc74672acf5
Issa Oulkhir <ioulkhir@e1r11p5.1337.ma>
2025-03-13 13:59:45 +0000

    nothing much

--- executer/execute.c
+++ executer/execute.c
@@ -6,7 +6,7 @@
 /*   By: ioulkhir <ioulkhir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/04 09:01:06 by ioulkhir          #+#    #+#             */
-/*   Updated: 2025/03/13 10:08:27 by ioulkhir         ###   ########.fr       */
+/*   Updated: 2025/03/13 13:59:20 by ioulkhir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,9 @@ void	execute(t_2_exec *data, t_env **my_env, t_garbage **my_garbage)
 			|| !ft_strcmp(data->cmd[0], "exit")
 			|| !ft_strcmp(data->cmd[0], "export")
 			|| !ft_strcmp(data->cmd[0], "unset")
+			|| !ft_strcmp(data->cmd[0], "pwd")
+			|| !ft_strcmp(data->cmd[0], "env")
+			|| !ft_strcmp(data->cmd[0], "echo")
 		)
 	)
 		(exec_builtin(data, my_env, my_garbage, 0), pi.process_num--);
