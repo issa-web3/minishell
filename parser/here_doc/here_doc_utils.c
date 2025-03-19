c2a09182cb84c29d3ee5259e3f169ea9126735b3
khalidoukoujane <oukoujanekhalid9@gmail.com>
2025-03-19 15:42:25 +0000

    added

--- parser/here_doc/here_doc_utils.c
+++ parser/here_doc/here_doc_utils.c
@@ -1,17 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   here_doc.c                                         :+:      :+:    :+:   */
+/*   here_doc_utils.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: khoukouj <khoukouj@student.42.fr>          #+#  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025-03-18 15:04:01 by khoukouj          #+#    #+#             */
-/*   Updated: 2025-03-18 15:04:01 by khoukouj         ###   ########ma        */
+/*   Created: 2025-03-19 15:19:21 by khoukouj          #+#    #+#             */
+/*   Updated: 2025-03-19 15:19:21 by khoukouj         ###   ########ma        */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../minishell.h"
 
-int	ft_handle_heredoc(t_token **tokens, t_2_exec *node, t_garbage **g)
+int	is_expand_heredoc(char *del)
 {
+	int	i;
+
+	i = 0;
+	if (!del)
+		return (0);
+	while (del[i])
+	{
+		if (is_quote(del[i]))
+			return (0);
+		i++;
+	}
+	return (1);
 }
