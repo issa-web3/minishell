e37e031ad9a9b198a78fd2e18dc558ce748fc970
Issa Oulkhir <ioulkhir@e1r3p5.1337.ma>
2025-04-08 18:11:51 +0100

    test

--- garbage/ft_malloc.c
+++ garbage/ft_malloc.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_malloc.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: test <test@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: ioulkhir <ioulkhir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/09 10:23:38 by ioulkhir          #+#    #+#             */
-/*   Updated: 2025/03/26 02:52:18 by test             ###   ########.fr       */
+/*   Updated: 2025/04/08 18:10:32 by ioulkhir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,16 +22,14 @@ void	*ft_malloc(size_t size, t_garbage **my_garbage)
 	{
 		perror("malloc");
 		clear_all(my_garbage);
-		set_exit_status(EXIT_FAILURE);
-		exit(EXIT_FAILURE);
+		set_and_exit(EXIT_FAILURE);
 	}
 	new_garbage = append_garbage(my_garbage, allocated);
 	if (new_garbage == NULL)
 	{
 		perror("malloc");
 		clear_all(my_garbage);
-		set_exit_status(EXIT_FAILURE);
-		exit(EXIT_FAILURE);
+		set_and_exit(EXIT_FAILURE);
 	}
 	new_garbage->ptr = allocated;
 	new_garbage->next = NULL;
