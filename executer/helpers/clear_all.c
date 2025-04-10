ed926588966138f7d97a7805140d1ee2332d9a52
Issa Oulkhir <ioulkhir@e2r6p5.1337.ma>
2025-04-10 10:36:07 +0100

    nothing much

--- executer/helpers/clear_all.c
+++ executer/helpers/clear_all.c
@@ -6,7 +6,7 @@
 /*   By: ioulkhir <ioulkhir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/21 16:22:27 by test              #+#    #+#             */
-/*   Updated: 2025/04/08 11:10:38 by ioulkhir         ###   ########.fr       */
+/*   Updated: 2025/04/10 10:33:56 by ioulkhir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,8 +27,11 @@ void	clear_all(t_garbage **my_garbage)
 		{
 			process_idx = data->p_info.process_idx - 1;
 			pipes = data->pipes;
-			close(pipes[process_idx][0]);
-			close(pipes[process_idx][1]);
+			if (pipes)
+			{
+				close(pipes[process_idx][0]);
+				close(pipes[process_idx][1]);
+			}
 		}
 		clear_env(my_env);
 	}
