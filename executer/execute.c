ca94e61ed84e86ad4522739cf40300ccad43edaf
Issa Oulkhir <ioulkhir@e2r2p3.1337.ma>
2025-04-14 15:44:22 +0100

    more clean code

--- executer/execute.c
+++ executer/execute.c
@@ -6,7 +6,7 @@
 /*   By: ioulkhir <ioulkhir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/04 09:01:06 by ioulkhir          #+#    #+#             */
-/*   Updated: 2025/04/14 14:20:12 by ioulkhir         ###   ########.fr       */
+/*   Updated: 2025/04/14 15:22:18 by ioulkhir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,22 +53,29 @@ void	execute(t_2_exec *data, t_env **my_env, t_garbage **my_garbage)
 
 	update_pwd(data, my_garbage, NULL);
 	set_exit_status(EXIT_SUCCESS);
+	
 	p_info.process_idx = 0;
-	p_info.process_num = ft_lstsize(data) - 1;
-	pipes = ft_malloc(p_info.process_num++ * sizeof(t_pipe), my_garbage);
-	data->pipes = pipes;
 	p_info.fork_response = 314;
+	p_info.process_num = ft_lstsize(data);
+
+	pipes = ft_malloc((p_info.process_num - 1) * sizeof(t_pipe), my_garbage);
+	data->pipes = pipes;
 	data->p_info = p_info;
 	if (is_a_parent_task(data, p_info))
 		(exec_builtin(data, my_env, my_garbage), p_info.process_num--);
 	fail = fork_and_pipe(pipes, &p_info);
-	close_useless_pipes(pipes, p_info.process_idx);
-	if (p_info.fork_response)
-		wait_last_pid(p_info.fork_response);
-	while (p_info.fork_response && wait(NULL) != -1)
-		;
 	if (fail)
 		return (clear_all(my_garbage), set_and_exit(EXIT_FAILURE));
-	data->p_info = p_info;
-	execute_task_by_idx(pipes, data, my_env, my_garbage);
+	if (p_info.process_num != 0)
+	{
+		close_useless_pipes(pipes, p_info.process_idx);
+		if (p_info.fork_response)
+		{
+			// wait_last_pid(p_info.fork_response);
+			while (wait(NULL) != -1)
+				;
+		}
+		data->p_info = p_info;
+		execute_task_by_idx(pipes, data, my_env, my_garbage);
+	}
 }
--- executer/helpers/clear_all.c
+++ executer/helpers/clear_all.c
@@ -6,7 +6,7 @@
 /*   By: ioulkhir <ioulkhir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/21 16:22:27 by test              #+#    #+#             */
-/*   Updated: 2025/04/10 10:33:56 by ioulkhir         ###   ########.fr       */
+/*   Updated: 2025/04/14 15:31:35 by ioulkhir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,24 +15,15 @@
 void	clear_all(t_garbage **my_garbage)
 {
 	t_pipe			*pipes;
-	int				process_idx;
-	t_2_exec		*data;
 	t_env			**my_env;
+	int				i;
 
 	if ((*my_garbage))
 	{
-		data = (*my_garbage)->data;
 		my_env = (*my_garbage)->my_env;
-		if (data)
-		{
-			process_idx = data->p_info.process_idx - 1;
-			pipes = data->pipes;
-			if (pipes)
-			{
-				close(pipes[process_idx][0]);
-				close(pipes[process_idx][1]);
-			}
-		}
+		i = 0;
+		while (i < 1024)
+			close(i++);
 		clear_env(my_env);
 	}
 	clear_garbage(my_garbage);
