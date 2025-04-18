20547497257c44accf5057be215eca479c5d29da
Issa Oulkhir <ioulkhir@e2r5p4.1337.ma>
2025-04-18 13:26:14 +0100

    clean repo

--- outsiders/compilation.bash
+++
@@ -1,6 +0,0 @@
-
-
-
-
-
-cc *.c */*.c */*/*.c  */*/*/*.c  -L/home/kali/.brew/opt/readline/lib -I/home/kali/.brew/opt/readline/include -g3 -fsanitize=address -lreadline -o minishell && ./minishell
--- outsiders/leaks.bash
+++
@@ -1,1 +0,0 @@
-valgrind --track-origins=yes --track-fds=yes --leak-check=full --show-leak-kinds=all ./minishell
