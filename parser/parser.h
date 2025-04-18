2cb95f424e6cc70848ec78ad326ea4b0ab3f0435
khalidoukoujane <oukoujanekhalid9@gmail.com>
2025-04-18 10:54:29 +0100

    norminette done

--- parser/parser.h
+++ parser/parser.h
@@ -18,8 +18,8 @@
 // the boss
 t_2_exec	*parsing(char *line, t_env **envl, t_garbage **garbage);
 //utils
-t_file	    *ft_lstlast_file(t_file *lst);
-void        ft_skip_spaces(char	*s, int *i);
+t_file		*ft_lstlast_file(t_file *lst);
+void		ft_skip_spaces(char	*s, int *i);
 int			is_whitespace(char c);
 void		sigint_handler(int sig);
 int			is_only_whitespace(char *str);
@@ -31,7 +31,7 @@ int			is_expand_heredoc(char *del);
 int			init_syntax_data(int *i, char *line);
 void		hide_quotes(t_token **node);
 void		restore_quotes(t_token **lst);
-char        *remove_quotes(char *str, t_garbage **g);
+char		*remove_quotes(char *str, t_garbage **g);
 int			is_operator(char c);
 void		ft_skip_quoted_section(char *str, int *i, char quote_type);
 char		*ft_strtrim(char *str, char *set, t_garbage **to_free);
@@ -43,10 +43,11 @@ int			can_i_expand(char *str);
 int			my_count_words(const char *str);
 //syntax error
 int			ft_check_syntax_error(char *line);
-int         ft_ambiguous_redirect(int a_r_found);
+int			ft_ambiguous_redirect(int a_r_found);
 // tokens
 t_token		*ft_create_tokens(char *str, t_env *env, t_garbage **garbage);
-void		ft_handle_cmd(t_2_exec **node, t_garbage **garbage, t_token **tokens);
+void		ft_handle_cmd(t_2_exec **node,
+				t_garbage **garbage, t_token **tokens);
 t_2_exec	*ft_analyse(char *line, t_env *envl, t_garbage **garbage);
 char		**extract(char **old_str, char *new, t_garbage **garbage);
 char		*ft_replace_dollar(char *str, t_env *env, t_garbage **garbage);
@@ -54,16 +55,18 @@ t_token		*remove_quotes_from_toks(t_token *toks, t_garbage **g);
 char		**ft_strtok(char *s, t_garbage **my_garbage);
 t_token		*ft_lstlast_token(t_token *lst);
 t_token		*new_token_node(char *str, int type, t_garbage **garbage);
-int	to_expand(t_token **node, t_env *env, t_garbage **g);
+int			to_expand(t_token **node, t_env *env, t_garbage **g);
 //expanding
-void		ft_expand_token(t_token **node, t_env *envl, t_garbage **garbage, int flag);
-void	restore_hidden_q(char **del);
+void		ft_expand_token(t_token **node, t_env *envl,
+				t_garbage **garbage, int flag);
+void		restore_hidden_q(char **del);
 t_expand	*ft_create_expand_node(char *s, t_garbage **garbage);
 void		ft_lstadd_back_t_expand(t_expand **lst, t_expand *new);
 //heredoc
-int	ft_handle_heredoc(t_token **tokens, t_2_exec **node, t_env *env, t_garbage **g);
-char	*generate_file_name(t_garbage	**g);
-t_file	*new_file_node(char *name, t_garbage **g, char *buff);
-void	ft_sigint_heredoc(int signo);
-void	create_heredoc_node(t_file	**node, t_garbage **g, char *buff);
+int			ft_handle_heredoc(t_token **tokens, t_2_exec **node,
+				t_env *env, t_garbage **g);
+char		*generate_file_name(t_garbage	**g);
+t_file		*new_file_node(char *name, t_garbage **g, char *buff);
+void		ft_sigint_heredoc(int signo);
+void		create_heredoc_node(t_file	**node, t_garbage **g, char *buff);
 #endif
