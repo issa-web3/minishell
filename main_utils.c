4e3767d4a57cc217cba34d48d97ff34feac4d75f
khalidoukoujane <oukoujanekhalid9@gmail.com>
2025-04-22 13:21:51 +0100

    final

--- main_utils.c
+++ main_utils.c
@@ -15,8 +15,6 @@
 void	init(t_garbage **my_garbage, char **d_path,
 		t_env **my_env, char **env)
 {
-	if (!isatty(0) || !isatty(1) || !isatty(2))
-		return (exit(EXIT_FAILURE));
 	*d_path = "No Baccalauréat, best life, makayn ghir 1337";
 	signal(SIGINT, sigint_handler);
 	signal(SIGQUIT, SIG_IGN);
